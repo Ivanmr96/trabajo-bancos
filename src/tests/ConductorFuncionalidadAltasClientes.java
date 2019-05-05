a1b27f78d66f4193faf529dd3d923d55cd78d05b
Ángela <34713438+randomkwiz@users.noreply.github.com>
2019-05-05 17:37:16 +0200

    Más comprobaciones

--- src/tests/ConductorFuncionalidadAltasClientes.java
+++ src/tests/ConductorFuncionalidadAltasClientes.java
@@ -6,16 +6,36 @@ public class ConductorFuncionalidadAltasClientes {
     public static void main(String[] args) {
         GestionBancoComercial g = new GestionBancoComercial();
 /*
-        g.solicitarAltaCliente("CAIXESBBXXX", "12345678-L",9999.9);
-        g.solicitarAltaCliente("CAIXESBBXXX", "22345678-L",499.9);
-        g.solicitarAltaCliente("CAIXESBBXXX", "32345678-L",5999.9);
-        g.solicitarAltaCliente("CAIXESBBXXX", "42345678-L",199.9);
+        g.solicitarAltaCliente("CAIXESBBXXX", "55345678-L",9999.9);
+        g.solicitarAltaCliente("CAIXESBBXXX", "66345678-L",499.9);
+        g.solicitarAltaCliente("CAIXESBBXXX", "77345678-L",5999.9);
+        g.solicitarAltaCliente("CAIXESBBXXX", "88345678-L",199.9);
+
 
 
 
         g.marcarCuentaComoBorrada("ESPCAIXESBBXXX0000001");
+        g.marcarCuentaComoBorrada("ESPCAIXESBBXXX0000002");
+        g.marcarCuentaComoBorrada("ESPCAIXESBBXXX0000003");
+
+
+ */
+
+/*
+        g.solicitarAltaCliente("CAIXESBBXXX", "123456789-P",100.9);
+        g.marcarCuentaComoBorrada("ESPCAIXESBBXXX0000001");
+        g.solicitarAltaCliente("CAIXESBBXXX", "123456789-I",200.9);
+        g.marcarCuentaComoBorrada("ESPCAIXESBBXXX0000003");
+        g.solicitarAltaCliente("CAIXESBBXXX", "123456789-R",300.9);
+        g.marcarCuentaComoBorrada("ESPCAIXESBBXXX0000002");
+        g.solicitarAltaCliente("CAIXESBBXXX", "123456789-T",400.9);
 */
-    g.aceptarAltasClientes("CAIXESBBXXX");
+
+//        g.marcarCuentaComoBorrada("ESPCAIXESBBXXX0000004");
+  //      g.marcarCuentaComoBorrada("ESPCAIXESBBXXX0000004");
+
+
+       // g.aceptarAltasClientes("CAIXESBBXXX");
 
        // String prueba = "SDFSDF,*";
         //System.out.println(prueba.contains("*"));
